f9517e4ff92a56394c00d90ff91c3689991054dc
krausjm7 <krausjm7@gmail.com>
2025-09-19 12:34:38 -0400

    Initial commit of user management service project setup

---
+++ src/services/userService.js
@@ -0,0 +1,7 @@
+// Service layer for user-related operations
+
+const User = require('../models/userModel');
+// Function to find user by ID
+exports.findUserById = async (userId) => {
+    return await User.findById(userId);
+};
---
+++ src/utils/errorHandler.js
@@ -0,0 +1,8 @@
+// Centralized error handling
+
+const logger = require('./logger');
+const errorHandler = (err, req, res, next) => {
+    logger.error(err);
+    res.status(500).json({ error: 'Something went wrong.' });
+};
+module.exports = errorHandler;
